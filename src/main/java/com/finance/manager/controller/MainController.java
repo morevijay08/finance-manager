a8925bca240accf1110eb746c566c8333a5b3996
Vijay Rajkumar More <vijaymore6212@gmail.com>
2026-08-28 14:07:54 +0530

    feat: add root view controller

---
+++ src/main/java/com/finance/manager/controller/MainController.java
@@ -0,0 +1,6 @@
+package com.finance.manager.controller;
+
+public class MainController {
+    // Root controller for the application shell.
+    // Screen-specific event handling will be added in later phases.
+}
