554852d254abd230c388443e0f6a56c40a13bf23
Vijay Rajkumar More <vijaymore6212@gmail.com>
2026-08-28 14:21:41 +0530

    fix: keep application styling during registration navigation

--- src/main/java/com/finance/manager/controller/RegisterController.java
+++ src/main/java/com/finance/manager/controller/RegisterController.java
@@ -101,8 +101,10 @@ public class RegisterController {
     private void switchScene(ActionEvent event, String resource) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
         Parent root = loader.load();
+        Scene scene = new Scene(root, 900, 600);
+        scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root, 900, 600));
+        stage.setScene(scene);
         stage.show();
     }
 }
